5ebcb5035f25a247f24e729c891bcfa5991f53b0
AlexandrDorosh <dovgaloleksandr388@gmail.com>
2021-08-26 12:08:34 +0300

    create hw-4

--- home-work-3/controllers/auth.controller.js
+++ home-work-3/controllers/auth.controller.js
@@ -8,6 +8,7 @@ module.exports = {
     loginUser: async (req, res) => {
         const { login, password } = req.body;
         const usersData = await authUserRead(usersDb);
+        console.log(usersData);
         const findUser = usersData.find((user) => user.login === login && user.password === password);
         if (findUser) {
             return res.render('hello', { findUser });
